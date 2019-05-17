5c0a5eb7c0500ae10212623f7b44e6c851612707
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-05-16 21:54:19 -0300

    Add Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductPageComponent } from './pages/store/product-page/product-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
